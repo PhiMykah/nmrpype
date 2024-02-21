86f153e78d9026bc0afeb8503038fcb6ac1b63bb
Micah Smith <mykahsmith21@gmail.com>
2024-02-21 09:47:42 -0500

    newfeatures version number change

--- nmrPype/setup.py
+++ nmrPype/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name='nmrPype',
-    version='0.8.0',
+    version='0.8.5',
     packages=find_packages(), 
     install_requires=[
         'numpy',
