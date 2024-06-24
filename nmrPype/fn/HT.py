cf7d98d22e0ede495fb91d8bb44e41b7ba7f1e30
Micah Smith <mykahsmith21@gmail.com>
2024-06-24 15:08:14 -0400

    HT starting implementation

--- nmrPype/fn/HT.py
+++ nmrPype/fn/HT.py
@@ -18,16 +18,8 @@ class HilbertTransform(Function):
     ----------
     ht_ps90_180 : bool, optional
         Enable mirror image Hilbert transform, by default False
-    ht_zf : bool, optional
-        Utilize a temporary zero fill for speed, by default False
     ht_td : bool, optional
         Set time domain size to SIZE/2, by default False
-    ht_auto : bool, optional
-        Enable auto mode; Selects HT and ZF mode, by default False
-    ht_ps0_0 : bool, optional
-        Auto mode option: Force ordinary Hilbert transform, by default False
-    ht_nozf : bool, optional
-        Disable temporary zero fill, by default False
     mp_enable : bool, optional
         Enable multiprocessing, by default False
     mp_proc : int, optional
@@ -35,23 +27,20 @@ class HilbertTransform(Function):
     mp_threads : int, optional
         Number of threads to utilize per process, by default 0
     """
-    def __init__(self, ht_ps90_180 : bool = False, ht_zf : bool = False,
-                 ht_td : bool = False, ht_auto : bool = False, 
-                 ht_ps0_0 : bool = False, ht_nozf : bool = False,
+    def __init__(self, ht_ps90_180 : bool = False, ht_td : bool = False, 
                  mp_enable : bool = False, mp_proc : int = 0, mp_threads : int = 0):
         
         self.ht_ps90_180 = ht_ps90_180
-        self.ht_zf = ht_zf
+        # self.ht_zf = ht_zf
         self.ht_td = ht_td
-        self.ht_auto = ht_auto
-        self.ht_ps0_0 = ht_ps0_0
-        self.ht_nozf = ht_nozf
+        # self.ht_auto = ht_auto
+        # self.ht_ps0_0 = ht_ps0_0
+        # self.ht_nozf = ht_nozf
 
         self.mp = [mp_enable, mp_proc, mp_threads]
         self.name = "HT"
         
-        params = {'ht_ps90_180':ht_ps90_180, 'ht_zf':ht_zf, 'ht_td':ht_td,
-                  'ht_auto':ht_auto, 'ht_ps0_0':ht_ps0_0, 'ht_nozf':ht_nozf}
+        params = {'ht_ps90_180':ht_ps90_180, 'ht_td':ht_td}
         super().__init__(params)
 
     ############
@@ -104,17 +93,31 @@ class HilbertTransform(Function):
         ndarray
             Updated array after function operation
         """
+        # Ensure support for an odd number of points
+
         it = np.nditer(array, flags=['external_loop','buffered'], op_flags=['readwrite'], buffersize=array.shape[-1], order='C')
         with it:
             for x in it:
                 if verb[0]:
                     Function.verbPrint('HT', it.iterindex, it.itersize, array.shape[-1], verb[1:])
-                    x[...] = hilbert(x.real)
+                    x[...] = self.hilb(x)
             if verb[0]:
                 print("", file=stderr)
 
         return array
     
+    def hilb(self, array : np.ndarray) -> np.ndarray:
+        x = array.real
+        htSize = len(x)
+
+        if (self.ht_ps90_180):
+            htSize = 2*htSize
+        elif (self.ht_td):
+            htSize = htSize/2
+        
+        return hilbert(x, htSize)
+
+
     ##################
     # Static Methods #
     ##################
@@ -136,16 +139,16 @@ class HilbertTransform(Function):
         HT = subparser.add_parser('HT', parents=[parent_parser], help='Perform a Hilbert Transform (HT) on the data')
         HT.add_argument('-ps90-180', action='store_true', 
                         dest='ht_ps90_180', help='Mirror Image Hilbert Transform.')
-        HT.add_argument('-zf', action='store_true', 
-                        dest='ht_zf', help='Temporary Zero Fill for Speed')
+        # HT.add_argument('-zf', action='store_true', 
+        #                 dest='ht_zf', help='Temporary Zero Fill for Speed')
         HT.add_argument('-td', action='store_true', 
                         dest='ht_td', help='Set Time-Domain Size to SIZE/2')
-        HT.add_argument('-auto', action='store_true', 
-                        dest='ht_auto', help='Auto Mode; Select HT and ZF mode.')
-        HT.add_argument('-ps0-0', action='store_true',
-                        dest='ht_ps0_0', help='Force Ordinary Hilbert Transform')
-        HT.add_argument('-nozf', action='store_true',
-                        dest='ht_nozf', help='No Temporary Zero Fill')
+        # HT.add_argument('-auto', action='store_true', 
+        #                 dest='ht_auto', help='Auto Mode; Select HT and ZF mode.')
+        # HT.add_argument('-ps0-0', action='store_true',
+        #                 dest='ht_ps0_0', help='Force Ordinary Hilbert Transform')
+        # HT.add_argument('-nozf', action='store_true',
+        #                 dest='ht_nozf', help='No Temporary Zero Fill')
         
 
     ####################
@@ -177,8 +180,21 @@ class HilbertTransform(Function):
         data : DataFrame
             Target data frame containing header to update
         """
-        # Update ndsize here  
-        pass
+        # Set data to complex if needed
+        currDim = data.getCurrDim()
+        if data.getParam("NDQUADFLAG", currDim) == 1:
+            data.setParam("NDQUADFLAG", currDim, 0.0)
+
+        shape = data.array.shape
+        qFlags = []
+        # Get the flags for all dimensions
+        for dim in range(len(shape)):
+            qFlags.append(data.getParam('NDQUADFLAG', dim+1))
+        
+        # Check if all dimensions are real
+        isReal = all(bool(flag) for flag in qFlags)
+
+        data.setParam('FDQUADFLAG', float(1) if isReal else float(0))
 
 
         
