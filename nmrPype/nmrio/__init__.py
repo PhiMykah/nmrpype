676791e943523f2d0d596d476caf451bab0ad087
Micah Smith <mykahsmith21@gmail.com>
2024-08-22 13:50:03 -0400

    1.0.0 nmrPype Release

--- nmrPype/nmrio/__init__.py
+++ nmrPype/nmrio/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 from .read import *
 from .write import *
 from .fileiobase import *
-from .ccp4.ccp4 import load_ccp4_map
+from .ccp4 import load_ccp4_map
 import io
 """
 nmrio
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name='nmrPype',
-    version='0.8.6',
+    version='1.0.0',
     packages=find_packages(), 
     install_requires=[
         'numpy',
